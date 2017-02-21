55618f4fccf2e3453049f95490609ea800e6c796
whitephenix13 <alexandrelasbleis@yahoo.fr>
2017-02-21 16:04:39 +0100

    2.2

--- main.m
+++ main.m
@@ -1,32 +1,7 @@
-test = 'gaussConv';%gaussDer gaussConv
-if(strcmp(test ,'gaussConv'))
-    kernel_size = [5, 10, 20, 30];
-    sigmas_x = [0.5, 5, 10, 20];
-    sigmas_y = [0.5, 5, 10, 20];
-    n = 4;
-    image = imread('cameraman.tif');
-
-    for i = 1:n
-        conv = gaussConv(image, sigmas_x(i), sigmas_y(i), kernel_size(i));
-        subplot((n+2)/2,(n+2)/2,i); 
-        imshow(conv, []);
-        tit = [num2str(sigmas_x(i)),' ',num2str(sigmas_y(i)),' ',num2str(kernel_size(i))];
-        title(tit);
-    end
-    
-    matlab_sigma = 20;
-    matlab_hsize = 30;
-    H = fspecial('gaussian',matlab_hsize,matlab_sigma);
-    res = imfilter(image, H);
-    subplot((n+2)/2,(n+2)/2,n+1);
-    imshow(res, []);
-    title(strcat('original matlab: ',num2str(matlab_sigma),'-', num2str(matlab_sigma),'-',num2str(matlab_hsize)));
-    
-elseif(strcmp(test,'gaussDer'))
-    image = imread('cameraman.tif');
-    sigma = 0.5;
-    kernel_size=5;
-    G  = gauss( sigma,kernel_size );
-    [conv, Gd ] = gaussDer( image, G, sigma );
-    imshow(conv, []);
+test = 'myHistMatching';%denoise myHistMatching 
+if(strcmp(test ,'denoise'))
+elseif(strcmp(test ,'myHistMatching'))
+    input = imread('Images/input.png');
+    reference = imread('Images/reference.png');
+    match_im = myHistMatching( input, reference );
 end
--- myHistMatching.m
+++ myHistMatching.m
@@ -1,7 +1,77 @@
 function [ imOut ] = myHistMatching( input, reference )
-%UNTITLED5 Summary of this function goes here
-%   Detailed explanation goes here
+    [H,W]= size(input);
+    imOut=zeros(size(input));
+    %get number of count per gray for input image
+    [input_count, input_gray]= imhist(input);
+    %get number of count per gray for reference image
+    [ref_count, ref_gray]= imhist(reference);
 
+    %Compute cumulative distribution functions F
+    F1 = cumsum(input_count);
+    %normalize it
+    F1 = F1 ./ F1(end);
+    
+    F2 = cumsum(ref_count);
+    %normalize it
+    F2 = F2 ./ F2(end);
+    
+    %Compute matching array M such that for a gray color G1, we find G2 such
+    %that M(G1) = G2 with F1(G1) = F2(G2)
+    M=zeros(size(F1));
+    g2 = 1 ;
+    for g1 = 1:length(M)
+        found=false; % keep track if we found matching value for g1
+        while (~found)
+            if(g2==1)
+                if(F1(g1)<F2(g2))
+                    M(g1)=0;
+                    found=true;
+                else
+                    g2=g2+1;
+                end
 
+            elseif(g2>=length(M))
+                M(g1) = length(M)-1;%=255
+                found=true;
+            elseif((F1(g1)>=F2(g2-1)) && (F1(g1)<F2(g2)))
+                M(g1) = g2-1;
+                found=true;
+            else
+                g2=g2+1;
+            end
+        end
+    end
+    %create tranformed image based on input image
+    for h=1:H
+        for w=1:W
+            imOut(h,w)= M(input(h,w)+1);
+        end
+    end
+    %recompute the count for the output image
+    out_count = zeros(size(input_count));
+    for i=1:length(input_count)
+        %note that M(i) in [0,255]
+        out_count(i)= input_count(M(i)+1);
+    end
+    %display reference image with histogram
+    figure;
+    subplot(1,2,1);
+    imshow(reference,[])
+    subplot(1,2,2);
+    stem(ref_gray,ref_count);
+    
+    %display input image with histogram
+    figure;
+    subplot(1,2,1);
+    imshow(input,[])
+    subplot(1,2,2);
+    stem(input_gray,input_count);
+    
+    %display result image with histogram
+    figure;
+    subplot(1,2,1);
+    imshow(imOut,[])
+    subplot(1,2,2);
+    stem(input_gray,out_count);
 end
 
