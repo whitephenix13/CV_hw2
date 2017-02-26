80d7f0eb11caaf9d5e9bd1917a94b47173ac57c2
Rezka Aufar Leonandya <rezka.aufar@gmail.com>
2017-02-26 01:05:48 +0100

    fix assignment 2

--- compute_gradient.m
+++ compute_gradient.m
@@ -7,7 +7,7 @@ sobel_y = [1,2,1;0,0,0;-1,-2,-1];
 gx = conv2(double(image),double(sobel_x));
 gy = conv2(double(image),double(sobel_y));
 im_magnitude = uint8(sqrt(gx.^2 + gy.^2));
-im_direction = uint8(atand(gx ./ gy));
+im_direction = uint8(atand(gy ./ gx));
 gx = uint8(gx);
 gy = uint8(gy);
 end
